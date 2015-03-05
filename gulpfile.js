126e96039dd4cfa655b6a50e413721a7a3cb5e5a
Ed Corcoran <ejcorcoran@me.com>
2015-03-05 16:55:19 +1100

    fixed mistake

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,59 @@
 // Dependencies
-var livereload = require('connect-livereload'),
-	express = require('express'),
-	gulp = require('gulp'),
-	autoprefixer = require('gulp-autoprefixer'),
-    imagemin = require('gulp-imagemin'),
-	concat = require('gulp-concat'),
-    cache = require('gulp-cache'),
-	rename = require('gulp-rename'),
-    sass = require('gulp-sass'),
-    shell = require('gulp-shell'),
-    uglify = require('gulp-uglify'),
-	gutil = require('gulp-util'),
-	tinylr = require('tiny-lr');
+var   livereload = require('connect-livereload'),
+	         del = require('del'),
+	     express = require('express'),
+	        gulp = require('gulp'),
+    autoprefixer = require('gulp-autoprefixer'),
+        imagemin = require('gulp-imagemin'),
+	    pngquant = require('imagemin-pngquant'),
+	      minify = require('gulp-minify-css'),
+	      concat = require('gulp-concat'),
+	       cache = require('gulp-cache'),
+	      rename = require('gulp-rename'),
+	        sass = require('gulp-sass'),
+	       shell = require('gulp-shell'),
+	      uglify = require('gulp-uglify'),
+	       gutil = require('gulp-util'),
+	      tinylr = require('tiny-lr');
 
 // Variables
 var express_port = 8888,
 	express_root = __dirname + '/dist/',
-	lr = void 0,
-	livereload_port = 35729,
-	dist = { stat: "dist/", css: "dist/css/", js: "dist/js/", img: "dist/img/", overlord: "dist/**/*.{js,html,css}" },
-	src = { stat: [ "src/*.html", "src/*.ico" ], sass: "src/sass/base.scss", js: "src/js/**/*.js", img: "src/img/**/*.{jpg,svg,png}" };
+			  lr = void 0,
+	     lr_port = 35729,
+
+			dist = {
+				stat: "dist/",
+				css: "dist/css/",
+				js: "dist/js/",
+				img: "dist/img/",
+				overlord: "dist/**/*.{js,html,css}"
+			},
+			 src = {
+				stat: [
+					"src/*.html",
+					"src/*.ico",
+					"src/video/*"
+				],
+				sass: "src/sass/base.scss",
+				js: [
+					"src/js/app.js"
+				],
+				img: "src/img/**/*.*"
+			};
+// */*.{jpg,svg,png,gif}
+
+// Clean
+gulp.task('clean', function(cb) {
+    del([dist.stat], cb);
+});
 
 // Server
 gulp.task('serve', function(event) {
 	// Server start console message
-	gutil.log.apply(gutil, [gutil.colors.black.bgCyan(' (ಠ_ಠ) The overlord is serving your site on http://localhost:' + express_port + ' ')]);
+	gutil.log.apply(gutil, [
+		gutil.colors.black.bgCyan(' (ಠ_ಠ) The overlord is serving your site on http://localhost:' + express_port + ' ')
+	]);
 
 	var app;
 	app = express();
@@ -33,8 +62,10 @@ gulp.task('serve', function(event) {
 	app.listen(express_port);
 
 	lr = tinylr();
-	lr.listen(livereload_port, function() {
-	  gutil.log.apply(gutil, [gutil.colors.black.bgCyan(' (ಠ_ಠ) The overlord is watching you on port ' + livereload_port + ' ')]);
+	lr.listen(lr_port, function() {
+		gutil.log.apply(gutil, [
+			gutil.colors.black.bgCyan(' (ಠ_ಠ) The overlord is watching you on port ' + lr_port + ' ')
+		]);
 	})
 });
 
@@ -43,7 +74,11 @@ var refresh = function(event) {
 	var fileName;
 	fileName = require('path').relative(express_root, event.path);
 
-	gutil.log.apply(gutil, [gutil.colors.black.bgCyan(' (ಠ_ಠ) '), gutil.colors.magenta(fileName), gutil.colors.cyan('changed')]);
+	gutil.log.apply(gutil, [
+		gutil.colors.black.bgCyan(' (ಠ_ಠ) '),
+		gutil.colors.magenta(fileName),
+		gutil.colors.cyan('changed')
+	]);
 
 	return lr.changed({
 		body: {
@@ -55,8 +90,9 @@ var refresh = function(event) {
 // SASS
 gulp.task("sass", function(event) {
 	return gulp.src(src.sass)
-	.pipe(sass({ outputStyle: 'compressed' }))
+	.pipe(sass({ outputStyle: 'nested' }))
 	.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(minify())
     .pipe(rename({ suffix: '.min' }))
 	.pipe(gulp.dest(dist.css));
 });
@@ -73,7 +109,18 @@ gulp.task('js', function(event) {
 // Images
 gulp.task('img', function(event) {
     return gulp.src(src.img)
-    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+    .pipe(cache(
+    	imagemin({
+    		optimizationLevel: 3,
+    		progressive: true,
+    		interlaced: true,
+    		svgoPlugins: [{
+    			removeViewBox: false
+    		}],
+    		use: [
+    			pngquant()
+			]
+		})))
     .pipe(gulp.dest(dist.img));
 });
 
@@ -87,6 +134,7 @@ gulp.task('copy', function() {
 gulp.task("watch", function() {
 	gulp.watch(src.sass, ["sass"]);
 	gulp.watch(src.js, ["js"]);
+	gulp.watch(src.img, ["img"]);
 	gulp.watch(src.stat, ["copy"]);
 	gulp.watch(dist.overlord, refresh);
 });
